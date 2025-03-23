c241cca661be81ad8865626fe645374e0d40ecad
Amr salah abd elghany <amr_salah150@yahoo.com>
2025-03-23 02:35:44 +0200

    Add files via upload

---
+++ frontend/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/*.test.js', '**/*.test.jsx'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+};
